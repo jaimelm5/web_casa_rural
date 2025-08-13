339c25ea3dc30e7c620323b114964c3fd8071a84
jaimelm5 <65109396+jaimelm5@users.noreply.github.com>
2025-08-13 20:23:51 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,16 +4,18 @@ const path = require('path');
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  // Movido de experimental a nivel raíz
+  outputFileTracingRoot: path.join(__dirname, '../'),
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    domains: ['i.pinimg.com'], // Para las imágenes de Pinterest
+  },
 };
 
 module.exports = nextConfig;
